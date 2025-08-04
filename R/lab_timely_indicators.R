232fdc767c071a5b5d57f06c2916c873c23b1e43
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-08-04 09:53:14 -0400

    Add documentation and yearly median calculation to lab_timely_indicators
    Added detailed roxygen documentation for the lab_timely_indicators function, including parameter and return descriptions. Updated the function to group and summarize lab data by region, country, and year, calculating yearly medians for timeliness intervals before aggregating three-year medians.

--- R/lab_timely_indicators.R
+++ R/lab_timely_indicators.R
@@ -1,3 +1,28 @@
+#' Calculate Lab Timeliness Indicators
+#'
+#' @description
+#' The function calculate the median timeliness interval for the current year up to the end date and
+#' the median of the previous three years.
+#'
+#' @details
+#' To request updated lab data, email Minh-Ly's group.
+#'
+#' The yearly median is only calculated up to the same month/day of the end date starting on Jan 1st.
+#' That is if the end date is 2025-08-15, then the 2025 median will be from 2025-01-01 to 2025-08-15,
+#' the 2024 median will be from 2024-01-01 to 2024-08-15. The three year median will be based on
+#' 2022-2024 medians.
+#'
+#' @param lab_data `tibble` Lab data.
+#' @param end_date `str` End date of the calculations. Defaults to `Sys.Date()`.
+#'
+#' @returns `tibble` A summary of median timeliness
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' lab_data <- readr::read_csv("lab_data.csv")
+#' lab_timely_indicators(lab_data)
+#' }
 lab_timely_indicators <- function(lab_data, end_date = Sys.Date()) {
   lab_data <- get_lab_intervals(lab_data)
   start_date_3_year <- lubridate::floor_date(end_date - lubridate::years(3), unit = "years")
@@ -10,6 +35,17 @@ lab_timely_indicators <- function(lab_data, end_date = Sys.Date()) {
       start_date_3_year,
       end_date_3_year
     )) |>
+    dplyr::group_by(whoregion, country, year) |>
+    dplyr::summarize(dplyr::across(
+      dplyr::any_of(c(
+        "days.lab.culture",
+        "days.culture.itd",
+        "days.seq.ship",
+        "days.seq.rec.res"
+      )),
+      \(x) median(x, na.rm = TRUE)
+    )) |>
+    dplyr::ungroup() |>
     dplyr::group_by(whoregion, country) |>
     dplyr::summarize(dplyr::across(
       dplyr::any_of(c(
