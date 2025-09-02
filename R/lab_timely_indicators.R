b734144c25bfb85bda4ce4dab6283d65e6ea5990
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-09-02 15:18:46 -0400

    Refactor lab timeliness median calculation logic
    Replaces in-line median calculations in lab_timely_indicators with a new helper function get_year_lab_median for improved modularity and clarity. The new approach iterates over indicator intervals, streamlining the computation of 3-year and current year medians, and simplifies the join logic for summary output.

--- R/lab_timely_indicators.R
+++ R/lab_timely_indicators.R
@@ -27,69 +27,36 @@ lab_timely_indicators <- function(lab_data, end_date = Sys.Date()) {
   lab_data <- get_lab_intervals(lab_data)
   start_date_3_year <- lubridate::floor_date(end_date - lubridate::years(3), unit = "years")
   end_date_3_year <- end_date - lubridate::years(1)
+  timely_intervals <- c(
+    "days.lab.culture",
+    "days.culture.itd",
+    "days.seq.ship",
+    "days.seq.rec.res"
+  )
 
-  # Calculate 3-year median
-  median_3_years <- lab_data |>
-    dplyr::filter(dplyr::between(
-      DateStoolCollected,
-      start_date_3_year,
-      end_date_3_year
-    )) |>
-    dplyr::group_by(whoregion, country, year) |>
-    dplyr::summarize(dplyr::across(
-      dplyr::any_of(c(
-        "days.lab.culture",
-        "days.culture.itd",
-        "days.seq.ship",
-        "days.seq.rec.res"
-      )),
-      \(x) median(x, na.rm = TRUE)
-    )) |>
-    dplyr::ungroup() |>
-    dplyr::group_by(whoregion, country) |>
-    dplyr::summarize(dplyr::across(
-      dplyr::any_of(c(
-        "days.lab.culture",
-        "days.culture.itd",
-        "days.seq.ship",
-        "days.seq.rec.res"
-      )),
-      \(x) median(x, na.rm = TRUE)
-    )) |>
-    tidyr::pivot_longer(cols =  dplyr::any_of(c(
-      "days.lab.culture",
-      "days.culture.itd",
-      "days.seq.ship",
-      "days.seq.rec.res"
-    )), names_to = "interval",
-    values_to = paste0(lubridate::year(start_date_3_year), "-",
-                       lubridate::year(end_date_3_year), " median"))
+  # Calculate 3-year medians
+  medians_3_years <- NULL
+  for (i in timely_intervals) {
+    indicator_median <- suppressMessages(get_year_lab_median(lab_data, i,
+                                               start_date_3_year,
+                                               end_date_3_year))
+    medians_3_years <- dplyr::bind_rows(medians_3_years, indicator_median)
 
-  # Calculate current year
-  median_current_year <- lab_data |>
-    dplyr::filter(dplyr::between(
-      DateStoolCollected,
-      lubridate::as_date(paste0(lubridate::year(end_date), "-01-01")),
-      end_date
-    )) |>
-    dplyr::group_by(whoregion, country) |>
-    dplyr::summarize(dplyr::across(
-      dplyr::any_of(c(
-        "days.lab.culture",
-        "days.culture.itd",
-        "days.seq.ship",
-        "days.seq.rec.res"
-      )),
-      \(x) median(x, na.rm = TRUE)
-    )) |>
-    tidyr::pivot_longer(cols =  dplyr::any_of(c(
-      "days.lab.culture",
-      "days.culture.itd",
-      "days.seq.ship",
-      "days.seq.rec.res"
-    )), names_to = "interval", values_to = paste0(lubridate::year(end_date), " median"))
+  }
 
-  lab_interval_summary <- dplyr::left_join(median_3_years, median_current_year)
+  # Current year
+  median_current_year <- NULL
+  for (i in timely_intervals) {
+    indicator_median <- suppressMessages(get_year_lab_median(lab_data, i,
+                                            lubridate::floor_date(end_date, unit = "year"),
+                                            end_date))
+    median_current_year <- dplyr::bind_rows(median_current_year, indicator_median)
+  }
+  # Rename to only have 1 year
+  colnames(median_current_year)[length(median_current_year)] <- paste0(lubridate::year(end_date), " median")
+
+  lab_interval_summary <- dplyr::left_join(medians_3_years, median_current_year,
+                                           by = dplyr::join_by(whoregion, country, interval))
 
   # Doing it this way because column names dynamically change but not the position
   lab_interval_summary["difference"] <- lab_interval_summary[5] - lab_interval_summary[4]
@@ -100,4 +67,81 @@ lab_timely_indicators <- function(lab_data, end_date = Sys.Date()) {
 
   return(lab_interval_summary)
 
+}
+
+# Private function
+
+#' Obtain the median of medians across years
+#'
+#' @description
+#' The function calculates the median interval for the lab timeliness indicators.
+#' It summarizes data at the yearly level and then takes the median/average of the years
+#' between the start and end dates.
+#'
+#'
+#' @param lab_data `tibble` Lab data obtained from WHO SLD team.
+#' @param indicator `str` Lab timeliness interval. Valid values are:
+#' - days.lab.culture
+#' - days.culture.itd
+#' - days.seq.ship
+#' - days.seq.rec.res
+#' @param start_date `str` Start date of the analysis. YYYY-MM-DD format.
+#' @param end_date `str` End date of the analysis. YYYY-MM-DD format.
+#'
+#' @returns `tibble` Summary of the median
+#' @keywords internal
+#'
+#' @examples
+get_year_lab_median <- function(lab_data, indicator, start_date, end_date) {
+
+  start_date <- lubridate::as_date(start_date)
+  end_date <- lubridate::as_date(end_date)
+
+  valid_indicators <- c(
+    "days.lab.culture",
+    "days.culture.itd",
+    "days.seq.ship",
+    "days.seq.rec.res"
+  )
+
+  if (!indicator %in% valid_indicators) {
+    cli::cli_alert_warning("Not a valid indicator. Please use: ")
+    cli::cli_li(valid_indicators)
+    cli::cli_abort("Pass a valid indicator and try again.")
+
+  }
+
+
+  # This is t1-t4, where TRUE values are valid dates
+  indicator_filter <- switch(
+    indicator,
+    "days.lab.culture" = "t1",
+    "days.culture.itd" = "t2",
+    "days.seq.ship" = "t3",
+    "days.seq.rec.res" = "t4"
+  )
+
+  summary <- lab_data |>
+    dplyr::filter(
+      dplyr::between(DateStoolCollected, start_date, end_date),
+      !!dplyr::sym(indicator_filter)
+    ) |>
+    dplyr::group_by(whoregion, country, year) |>
+    dplyr::summarize(dplyr::across(
+      dplyr::any_of(indicator),
+      \(x) median(x, na.rm = TRUE)
+    )) |>
+    dplyr::ungroup() |>
+    dplyr::group_by(whoregion, country) |>
+    dplyr::summarize(dplyr::across(
+      dplyr::any_of(indicator),
+      \(x) median(x, na.rm = TRUE)
+    )) |>
+    tidyr::pivot_longer(cols =  dplyr::any_of(indicator),
+                        names_to = "interval",
+    values_to = paste0(lubridate::year(start_date), "-",
+                       lubridate::year(end_date),
+                       " median"))
+
+  return(summary)
 }
