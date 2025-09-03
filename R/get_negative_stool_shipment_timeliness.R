57fbb21e1f2525727986dc91f46a251a72231e4d
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-09-03 10:17:59 -0400

    Add function to calculate negative stool shipment timeliness
    Introduces get_negative_lab_processing_timeliness(), which computes the median interval from collection to final results for negative samples, grouped by region, country, category, year, and month. The function supports analysis for the current and previous year.

---
+++ R/get_negative_stool_shipment_timeliness.R
@@ -0,0 +1,32 @@
+#' Obtain the timeliness interval between collection to final results of negative samples
+#'
+#' @description
+#' Calculates the median timeliness of negative samples between collection date to final lab results.
+#' The function will calculate for the year specified in the end date and the previous year.
+#'
+#' @param lab_data `tibble` Lab data.
+#' @param end_date `str` End date of the analysis. Defaults to the current date.
+#'
+#' @returns `tibble` Summary table of timeliness
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_negative_lab_processing_timeliness(lab_data)
+#' }
+get_negative_lab_processing_timeliness <- function(lab_data, end_date = Sys.Date()) {
+  summary <- lab_data |>
+    dplyr::mutate(month = lubridate::month(CaseDate, label = TRUE),
+                  days.collect.rec.lab = as.numeric(DateStoolReceivedinLab - DateStoolCollected)) |>
+    dplyr::filter(year >= lubridate::year(end_date) - 1,
+                  !is.na(days.collect.rec.lab),
+                  dplyr::between(days.collect.rec.lab, 0, 365),
+                  FinalCellCultureResult %in% c("Negative", "NPEV", NA)
+                  ) |>
+    dplyr::group_by(whoregion, country, culture.itd.cat, year, month) |>
+    dplyr::summarize(median = median(days.collect.rec.lab, na.rm = TRUE), .groups = "drop") |>
+    dplyr::arrange(year) |>
+    tidyr::pivot_wider(names_from = year, values_from = median)
+
+  return(summary)
+}
