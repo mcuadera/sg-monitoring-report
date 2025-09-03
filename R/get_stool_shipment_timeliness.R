93c21e1edbbc7dbe5b4309636ffe8e61acc47960
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-09-03 15:39:05 -0400

    Update get_stool_shipment_timeliness.R

--- R/get_stool_shipment_timeliness.R
+++ R/get_stool_shipment_timeliness.R
@@ -22,12 +22,15 @@ get_stool_shipment_timeliness <- function(lab_data, end_date = Sys.Date()) {
     dplyr::mutate(month = lubridate::month(CaseDate, label = TRUE),
                   days.collect.rec.lab = as.numeric(DateStoolReceivedinLab - DateStoolCollected)) |>
     dplyr::filter(year >= lubridate::year(end_date) - 1,
+                  dplyr::between(month, "Jan", lubridate::month(end_date, label = TRUE)),
                   !is.na(days.collect.rec.lab),
                   dplyr::between(days.collect.rec.lab, 0, 365)) |>
     dplyr::group_by(whoregion, country, culture.itd.cat, year, month) |>
     dplyr::summarize(median = median(days.collect.rec.lab, na.rm = TRUE), .groups = "drop") |>
     dplyr::arrange(year) |>
     tidyr::pivot_wider(names_from = year, values_from = median)
+
+  cli::cli_alert_info("Note: If end date is not the month end, comparisons from the previous year may be inaccurate")
 
   return(summary)
 
