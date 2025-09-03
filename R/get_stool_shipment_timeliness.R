56328bc52b38ad59acaecc319d35d19456f6a924
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-09-03 10:06:45 -0400

    Add function to calculate stool shipment timeliness
    Introduces get_stool_shipment_timeliness(), which computes the median time from stool collection to lab receipt, grouped by region, country, category, year, and month. The function filters and summarizes lab data for timeliness analysis.

---
+++ R/get_stool_shipment_timeliness.R
@@ -0,0 +1,34 @@
+#' Get timeliness of stool collection to shipment
+#'
+#' @description
+#' Calculates the median timeliness from the year specified at the end date for
+#' the time interval between stool collection to arrival in lab.
+#'
+#' @param lab_data `tibble` Lab data.
+#' @param end_date `str`End date of the analysis. Defaults to current date.
+#'
+#' @returns `tibble` Summary of stool collection to shipment timeliness
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_stool_shipment_timeliness(lab_data)
+#' }
+get_stool_shipment_timeliness <- function(lab_data, end_date = Sys.Date()) {
+
+  end_date <- lubridate::as_date(end_date)
+
+  summary <- lab_data |>
+    dplyr::mutate(month = lubridate::month(CaseDate, label = TRUE),
+                  days.collect.rec.lab = as.numeric(DateStoolReceivedinLab - DateStoolCollected)) |>
+    dplyr::filter(year >= lubridate::year(end_date) - 1,
+                  !is.na(days.collect.rec.lab),
+                  dplyr::between(days.collect.rec.lab, 0, 365)) |>
+    dplyr::group_by(whoregion, country, culture.itd.cat, year, month) |>
+    dplyr::summarize(median = median(days.collect.rec.lab, na.rm = TRUE), .groups = "drop") |>
+    dplyr::arrange(year) |>
+    tidyr::pivot_wider(names_from = year, values_from = median)
+
+  return(summary)
+
+}
